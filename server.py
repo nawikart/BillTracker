1ac701c65e87ebc9db9ca479b4b7e4931619d44b
nawikart <nawi@outpost-asia.com>
2018-06-05 14:48:38 +0800

    add edit

--- server.py
+++ server.py
@@ -8,14 +8,12 @@ def index():
     all_bills = get_all_bills()
     return render_template('index.html', all_bills = all_bills)
 
+
 @app.route('/add_bill', methods=['POST'])
 def add_bill():
     description = request.form['description']
     amount = request.form['amount']
-
-
     create_bill(amount, description)
-
     return redirect(url_for('index'))
 
 
@@ -24,5 +22,15 @@ def delete(bill_id):
     delete_bill(bill_id)
     return redirect(url_for('index'))
 
+
+@app.route('/edit_bill/<bill_id>', methods=['POST', 'GET'])
+def edit_bill(bill_id):
+    if request.method == 'POST':
+        update_bill(bill_id, request.form['amount'], request.form['description'])
+        return redirect(url_for('index'))
+
+    bill = get_bill(bill_id)
+    all_bills = get_all_bills()
+    return render_template('edit.html', bill = bill, all_bills = all_bills)
 
 app.run(debug=True)
