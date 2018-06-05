d4822694327c5c3b63ea0c40e625e81669cbed0e
nawikart <nawi@outpost-asia.com>
2018-06-05 12:14:46 +0800

    do redirect to index after save

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, flash, session, request, redirect, render_template
+from flask import Flask, flash, session, request, redirect, render_template, url_for
 from db.data_layer import get_all_bills, get_bill, create_bill, delete_bill, update_bill
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    all_bills = get_all_bills()
+    return render_template('index.html', all_bills = all_bills)
 
 @app.route('/add_bill', methods=['POST'])
 def add_bill():
@@ -14,8 +15,7 @@ def add_bill():
 
 
     create_bill(amount, description)
-
-    all_bills = get_all_bills()
-    return render_template('index.html', all_bills = all_bills)
+    
+    return redirect(url_for('index'))
 
 app.run(debug=True)
